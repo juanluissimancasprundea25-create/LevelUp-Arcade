d22ee87cd127fe649657a3330eafc7d3f829fb53
juanluissimancasprundea25-create <juanluis.simancasprundea.25@campusfp.es>
2026-05-20 20:03:00 +0200

    chore: estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/leveluparcade/LevelupArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.leveluparcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupArcadeApplication.class, args);
+	}
+
+}
