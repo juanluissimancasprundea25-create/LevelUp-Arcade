4d27d009b3226e65163167be02e23fe4e12fc568
Sergiomonago <sergio.monagomoreno.23@campusfp.es>
2026-05-26 09:57:00 +0200

    fix: tests no longer require env vars by using test profile

--- src/test/java/com/leveluparcade/LevelupArcadeApplicationTests.java
+++ src/test/java/com/leveluparcade/LevelupArcadeApplicationTests.java
@@ -2,12 +2,14 @@ package com.leveluparcade;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LevelupArcadeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
